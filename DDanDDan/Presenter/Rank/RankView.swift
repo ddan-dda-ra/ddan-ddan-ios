920c9c3d1e32729ec15b0c0829c745ef36797c6f
Zoe <mam07065@naver.com>
2025-03-01 16:35:46 +0900

    랭킹 뷰 파일 추가

---
+++ DDanDDan/Presenter/Rank/RankView.swift
@@ -0,0 +1,18 @@
+//
+//  RankView.swift
+//  DDanDDan
+//
+//  Created by 이지희 on 3/1/25.
+//
+
+import SwiftUI
+
+struct RankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RankView()
+}
