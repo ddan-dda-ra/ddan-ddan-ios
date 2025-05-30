a7c615414638fcec46a62c2e66595fad6ded800e
Zoe <mam07065@naver.com>
2025-05-30 17:56:10 +0900

    위젯 Extension 추가

---
+++ DDanDDan_Widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  DDanDDan_Widget
+//
+//  Created by 이지희 on 5/30/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
