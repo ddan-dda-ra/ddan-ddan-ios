e836b9afb3052727b68fa1810b2aa974af2d7919
Zoe <mam07065@naver.com>
2025-09-09 00:28:06 +0900

    코치마크뷰 표출을 위한 Subject 연결

--- DDanDDan/Presenter/Home/Views/Challenge/NewPetViewModel.swift
+++ DDanDDan/Presenter/Home/Views/Challenge/NewPetViewModel.swift
@@ -5,4 +5,15 @@
 //  Created by 이지희 on 9/9/25.
 //
 
-import Foundation
+import Swift
+import Combine
+
+
+final class NewPetViewModel: ObservableObject {
+    let dismissPublisher = PassthroughSubject<Void, Never>()
+    
+    func tapDisMissButton() {
+        dismissPublisher.send()
+    }
+}
+
