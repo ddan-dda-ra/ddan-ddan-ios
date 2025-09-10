7fd771d5058326d2a38fc3faa629c2643700c3a8
hwikang <khdrogba@gmail.com>
2025-09-10 16:35:07 +0900

    card UI 구현

--- DDanDDan/Entity/Home/HomeModel.swift
+++ DDanDDan/Entity/Home/HomeModel.swift
@@ -42,6 +42,15 @@ extension PetType {
         }
     }
     
+    var cardBackgroundImage: Image {
+        switch self {
+        case .pinkCat: return Image(.catCardBackground).resizable()
+        case .greenHam: return Image(.hamsterCardBackground).resizable()
+        case .purpleDog: return Image(.dogCardBackground).resizable()
+        case .bluePenguin: return Image(.penguinCardBackground).resizable()
+        }
+    }
+    
     func lottieString(level: Int, mode: LottieMode = .normal) -> String {
         let safeLevel = min(level, 5)
         
--- DDanDDan/Presenter/Components/FriendCard/FriendCardView.swift
+++ DDanDDan/Presenter/Components/FriendCard/FriendCardView.swift
@@ -7,6 +7,7 @@
 
 import ComposableArchitecture
 import SwiftUI
+import Lottie
 
 struct FriendCardEntity: Equatable {
     let userID: String
@@ -19,28 +20,24 @@ struct FriendCardEntity: Equatable {
 
 struct FriendCardView: View {
     let store: StoreOf<FriendCardReducer>
-    
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         ZStack {
-            Rectangle().fill(.clear).background(.thinMaterial)
-                .ignoresSafeArea()
-            
             cardView
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(BackgroundBlurView())
     }
     
     var cardView: some View {
-        VStack {
-            VStack {
-                store.entity.mainPetType.seBackgroundImage
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 296, height: 200)
+        VStack(spacing: 20) {
+            VStack(spacing: 0) {
                 
-                
+                cardImageView
+                cardContentView
                 
             }
-            .background(Color.elevationLevel03)
+            .background(Color.elevationLevel01)
             .clipShape(RoundedRectangle(cornerRadius: 16))
             
             Button(action: {}) {
@@ -50,25 +47,92 @@ struct FriendCardView: View {
             }
             .frame(width: 136, height: 56)
             .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+
         }
         .frame(width: 296, height: 489)
-
+    }
+    
+    var cardImageView: some View {
+        store.entity.mainPetType.cardBackgroundImage
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 296, height: 200)
+            .overlay(alignment: .bottom) {
+                LottieView(
+                    animation: .named(store.entity.mainPetType.lottieString(level: store.entity.petLevel)))
+                .playing(loopMode: .loop)
+                .frame(width: 100, height: 100)
+                .padding(.bottom, 23)
+            }
+            .overlay(alignment: .topTrailing) {
+                Image(.close)
+                    .padding(16)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
     }
     
     var cardContentView: some View {
-        VStack {
+        VStack(spacing: 0) {
             
-            HStack {
+            HStack(spacing: 8) {
                 Text(store.entity.userName)
+                    .font(.neoDunggeunmo22)
+                    .foregroundStyle(.textHeadlinePrimary)
                 
-                Text("LV. \(store.entity.petLevel)")
+                Text("LV.\(store.entity.petLevel)")
+                    .font(.neoDunggeunmo16)
+                    .foregroundStyle(.textHeadlinePrimary)
+                    .padding(.horizontal, 8)
+                    .padding(.top, 4)
+                    .padding(.bottom, 2)
+                    .background(.elevationLevel02)
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
             }
+            HStack(spacing: 0) {
+                Image(.fire)
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Text("받은 응원")
+                    .font(.body1_regular16)
+                    .foregroundStyle(.textBodyTeritary)
+                    .padding(.leading, 2)
+                
+                Text("\(store.entity.cheerCount)")
+                    .font(.neoDunggeunmo24)
+                    .foregroundStyle(.textBodyTeritary)
+                    .padding(.leading, 8)
+            }
+            .padding(.vertical, 4)
+            .padding(.horizontal, 12)
+            .background(.elevationLevel02)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .padding(.top, 13)
             
-            Text("받은 응원 \(store.entity.cheerCount)")
+            Rectangle()
+                .frame(height: 1)
+                .background(.elevationLevel02)
+                .padding(.vertical, 20)
             
-            Text("오늘 소모 칼로리")
-            Text("\(store.entity.totalCalories) kcal")
+            VStack(spacing: 3) {
+                Text("오늘 소모 칼로리")
+                    .font(.body1_regular16)
+                    .foregroundStyle(.textBodyTeritary)
+                HStack(spacing: 0) {
+                    Text("\(store.entity.totalCalories)")
+                        .font(.neoDunggeunmo20)
+                        .foregroundStyle(.textHeadlinePrimary)
+                    Text("kcal")
+                        .font(.neoDunggeunmo14)
+                        .foregroundStyle(.textHeadlinePrimary)
+                }
+                
+            }
         }
+        .padding(.vertical, 28)
+        .padding(.horizontal, 20)
     }
 }
 
