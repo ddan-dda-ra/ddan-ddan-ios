4d3fba2bb72911d1bba5e28b839ecdffcc4eb509
hwikang <khdrogba@gmail.com>
2025-10-18 15:59:33 +0900

    펫보관함 상단 보더안잘리도록 수정

--- DDanDDan/Presenter/Setting/Archive/PetArchiveView.swift
+++ DDanDDan/Presenter/Setting/Archive/PetArchiveView.swift
@@ -22,7 +22,7 @@ struct PetArchiveView: View {
             Color(.backgroundBlack)
                 .ignoresSafeArea()
             
-            VStack {
+            VStack(spacing: 0) {
                 CustomNavigationBar(
                     title: "펫 보관함",
                     leftButtonImage: Image(.arrow),
@@ -32,41 +32,7 @@ struct PetArchiveView: View {
                 )
                 .padding(.bottom, 28)
                 ScrollView {
-                    HStack {
-                        LazyVGrid(columns: columns, spacing: 20) {
-                            ForEach(0..<viewModel.gridItemCount, id: \.self) { index in
-                                ZStack {
-                                    let pet = viewModel.petList[safe: index]
-                                    RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
-                                        .stroke(viewModel.selectedIndex == index ? Color.buttonGreen : Color.clear, lineWidth: 4)
-                                        .background(
-                                            RoundedRectangle(cornerRadius: 8).foregroundColor(.borderGray)
-                                        )
-                                        .frame(width: 100, height: 100)
-                                    
-                                    if let pet = pet {
-                                        Image(pet.type.image(for: pet.level))
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(maxWidth: 80, maxHeight: 80)
-                                    } else {
-                                        Image(.questionMark)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 48, height: 48)
-                                            .padding(24)
-                                    }
-                                }
-                                .onTapGesture {
-                                    if viewModel.petList[safe: index] == nil {
-                                        viewModel.showToastMessage()
-                                    } else {
-                                        viewModel.toggleSelection(for: index)
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    petVGrid
                 }
                 .padding(.horizontal, 20)
                 Spacer()
@@ -98,6 +64,43 @@ struct PetArchiveView: View {
             if newValue {
                 coordinator.triggerHomeUpdate(trigger: true)
                 coordinator.pop()
+            }
+        }
+    }
+    
+    var petVGrid: some View {
+        LazyVGrid(columns: columns, spacing: 20) {
+            ForEach(0..<viewModel.gridItemCount, id: \.self) { index in
+                ZStack {
+                    let pet = viewModel.petList[safe: index]
+                    RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
+                        .strokeBorder(viewModel.selectedIndex == index ? Color.buttonGreen : Color.clear, lineWidth: 4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8).foregroundColor(.borderGray)
+                        )
+                        .frame(width: 100, height: 100)
+                        
+                    
+                    if let pet = pet {
+                        Image(pet.type.image(for: pet.level))
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 80, maxHeight: 80)
+                    } else {
+                        Image(.questionMark)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 48, height: 48)
+                            .padding(24)
+                    }
+                }
+                .onTapGesture {
+                    if viewModel.petList[safe: index] == nil {
+                        viewModel.showToastMessage()
+                    } else {
+                        viewModel.toggleSelection(for: index)
+                    }
+                }
             }
         }
     }
